082920dd332a3b530cd06d6e0b204fe98cb6d880
titan <titan@titan-B490>
2016-05-16 13:18:27 -0300

    more modifications

--- src/theordersystem/ui/controlers/FXMLCustomerManagerController.java
+++ src/theordersystem/ui/controlers/FXMLCustomerManagerController.java
@@ -7,15 +7,60 @@ package theordersystem.ui.controlers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
  *
- * @author Gabriel Longo P. Abreu
- * @author Aleksander Silva
+ * @author titan
  */
 public class FXMLCustomerManagerController implements Initializable {
+
+    @FXML
+    private Button btnew;
+    @FXML
+    private Button btmodify;
+    @FXML
+    private Button btapagar;
+    @FXML
+    private Button btconfirmar;
+    @FXML
+    private Button btcancelar;
+    @FXML
+    private BorderPane pnlateral;
+    @FXML
+    private TextField txpesquisa;
+    @FXML
+    private Button btpesquisar;
+    @FXML
+    private TableView<?> tableCustomers;
+    @FXML
+    private TableColumn<?, ?> colcod;
+    @FXML
+    private TableColumn<?, ?> colnome;
+    @FXML
+    private AnchorPane pndados;
+    @FXML
+    private TextField txcodigo;
+    @FXML
+    private TextField txnome;
+    @FXML
+    private TextField txpeso;
+    @FXML
+    private DatePicker dtdtnasc;
+    @FXML
+    private ComboBox<?> cbraca;
 
     /**
      * Initializes the controller class.
@@ -24,5 +69,33 @@ public class FXMLCustomerManagerController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void evtBtNovo(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtBtAlterar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtBtApagar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtBtConfirmar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtBtCancelar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtBtPesquisar(ActionEvent event) {
+    }
+
+    @FXML
+    private void evtClickTabela(MouseEvent event) {
+    }
     
 }
